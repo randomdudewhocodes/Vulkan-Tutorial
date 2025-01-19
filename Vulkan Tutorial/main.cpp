8c2b547bdfbefc3bb9a4eb2efd6bee5c7ad3129f
randomdudewhocodes <nguyen07112007@gmail.com>
2025-01-19 22:10:36 +0700

    INSTANCE!!!

--- Vulkan Tutorial/main.cpp
+++ Vulkan Tutorial/main.cpp
@@ -18,6 +18,8 @@ public:
 private:
     GLFWwindow* window;
 
+    VkInstance instance;
+
     void initWindow()
     {
         // initializes the GLFW library
@@ -35,7 +37,7 @@ private:
 
     void initVulkan()
     {
-
+        createInstance();
     }
 
     void mainLoop()
@@ -46,11 +48,47 @@ private:
 
     void cleanup()
     {
+        // destroy the instance
+        vkDestroyInstance(instance, nullptr);
+
         // destroying the window
         glfwDestroyWindow(window);
 
         // terminating GLFW
         glfwTerminate();
+    }
+
+    // Instance
+    void createInstance()
+    {
+        // fill in a struct with some information about our application
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hi :)";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        // tells the driver which global extensions and validation layers to use
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        // create an instance and check if the instance was created successfully
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to create instance!");
+        }
     }
 };
 
